5d11920b980b0974c74e3e0d62ed30a6e2c2b521
woosang <wooju9942@gmail.com>
2021-07-06 13:10:08 +0900

    day02_review

--- java_study/src/study1.java
+++ java_study/src/study1.java
@@ -1,5 +1,8 @@
 public class study1 {
     public static void main(String[] args) {
-        System.out.println("study1");
+        int intValue = 1000;
+        String strValue = "1000";
+
+        System.out.println(strValue);
     }
 }
