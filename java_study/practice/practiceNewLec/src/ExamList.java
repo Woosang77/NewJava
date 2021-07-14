2c0bf244f3a9c96cd4965ef8154742fbc8456318
woosang <wooju9942@gmail.com>
2021-07-14 14:50:04 +0900

    java_newlec_study

--- java_study/practice/practiceNewLec/src/ExamList.java
+++ java_study/practice/practiceNewLec/src/ExamList.java
@@ -1,4 +1,78 @@
+import java.util.Scanner;
+
 public class ExamList {
-    Exam[] exams;
-    int current;
+    private Exam[] exams;
+    private int current;
+
+    public ExamList() {
+        this(3);
+    }
+
+    public ExamList(int size){
+        exams = new Exam[size];
+        current = 0;
+    }
+
+    public void inputList() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----------성적 입력---------");
+        int kor, eng, math;
+        do {
+            System.out.printf("국어: ");
+            kor = scan.nextInt();
+            if (kor < 0 || 100 < kor) {
+                System.out.println("성적 범위 초과");
+            }
+        } while (kor < 0 || 100 < kor);
+        do {
+            System.out.printf("영어: ");
+            eng = scan.nextInt();
+            if (eng < 0 || 100 < eng) {
+                System.out.println("성적 범위 초과");
+            }
+        } while (eng < 0 || 100 < eng);
+        do {
+            System.out.printf("수학: ");
+            math = scan.nextInt();
+            if (math < 0 || 100 < math) {
+                System.out.println("성적 범위 초과");
+            }
+        } while (math < 0 || 100 < math);
+
+        Exam exam = new Exam(kor, eng, math);
+
+        if (exams.length == current){
+            Exam[] temp = new Exam[current + 5];
+            for (int i = 0; i < current; i++) {
+                temp[i] = exams[i];
+                exams = temp;
+            }
+        }
+        exams[current] = exam;
+        current++;
+    }
+
+    public void printList(){ printList(current); }
+
+    public void printList(int size){
+        System.out.println("----------성적 출력---------");
+
+        for (int i = 0; i < size; i++) {
+            Exam exam = exams[i];
+            int kor = exam.getKor();
+            int eng = exam.getEng();
+            int math = exam.getMath();
+
+            int total = exam.total();
+            float avg = exam.avg();
+
+            System.out.printf("국어: %d\n",kor);
+            System.out.printf("영어: %d\n", eng);
+            System.out.printf("수학: %d\n", math);
+            System.out.printf("총점 : %3d\n", total);
+            System.out.printf("평균 : %6.2f\n", avg);
+            System.out.println("--------------------------");
+        }
+    }
+
 }
--- java_study/practice/practiceNewLec/src/Program.java
+++ java_study/practice/practiceNewLec/src/Program.java
@@ -3,64 +3,41 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        int[] kors = new int[3];
-        int total = 0;
-        float avg;
+        ExamList list = new ExamList();
+
         int menu;
         boolean keepLoop = true;
-        Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
-            kors[i] = 0;
-        }
-
-        종료:
         while (keepLoop) {
-            //----------메인 메뉴------------------------------
-            System.out.println("----------메인 메뉴---------");
-            System.out.println("\t1.성적입력");
-            System.out.println("\t2.성적출력");
-            System.out.println("\t3.종료");
-            System.out.print("\t>");
-            menu = scan.nextInt();
-
-            switch (menu){
+            menu = inputMenu();
+            switch (menu) {
                 case 1:
-                    //------성적입력------------------------------------
-                    System.out.println("----------성적 입력---------");
-                    for (int i = 0; i < 3; i++) {
-                        do {
-                            System.out.printf("국어 %d :", i+1);
-                            kors[i] = scan.nextInt();
-                            if (kors[i] < 0 || 100 < kors[i]) {
-                                System.out.println("성적 범위 초과");
-                            }
-                        } while (kors[i] < 0 || 100 < kors[i]);
-                    }
+                    list.inputList();
                     break;
                 case 2:
-                    //-------성적 출력-----------------------------------------
-                    for (int i = 0; i <3; i++) {
-                        total += kors[i];
-                    }
-                    avg = total / 3.0f;
-
-                    System.out.println("----------성적 출력---------");
-                    for (int i = 0; i < 3; i++) {
-                        System.out.printf("국어%d : %3d\n", i + 1, kors[i]);
-                    }
-                    System.out.printf("총점 : %3d\n", total);
-                    System.out.printf("평균 : %6.2f\n", avg);
-                    System.out.println("--------------------------");
+                    list.printList(2);
                     break;
                 case 3:
                     System.out.println("시스템 종료");
                     keepLoop = false;
-                    break 종료;
+                    break;
                 default:
                     System.out.println("입력 오류 1~3까지 입력 가능");
             }
         }
-
     }
+
+    private static int inputMenu(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----------메인 메뉴---------");
+        System.out.println("\t1.성적입력");
+        System.out.println("\t2.성적출력");
+        System.out.println("\t3.종료");
+        System.out.print("\t>");
+        int menu = scan.nextInt();
+
+        return menu;
+    }
+
+
 }
